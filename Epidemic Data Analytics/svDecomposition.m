5a418a8ef3fc74540e36f90840dd09e03d633a74
uday kumar macherla <uday.89.kumar@gmail.com>
2014-10-31 09:46:37 -0700

    Pushing Matlab Function

---
+++ Epidemic Data Analytics/svDecomposition.m
@@ -0,0 +1,12 @@
+function [U, S, V] = svDecomposition(r)
+    fileName = './Data/X.csv';
+    X = csvread(fileName);
+    [U, S, V] = svd(X);
+    %Using only 'r' Semantics
+    [m,n] = size(X);
+    if(r<m && r<n)
+        csvwrite('./Data/U.csv',U(:,1:r));
+        csvwrite('./Data/S.csv',S(1:r,1:r));
+        csvwrite('./Data/V.csv',V(1:r,:));
+    end
+end

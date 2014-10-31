479831ae3673b54a758c2fa74a05fc88eed3f636
Karan Kothari <khkothar@asu.edu>
2014-10-30 23:19:53 -0700

    changed getScore signature

--- Epidemic Data Analytics/src/edu/asu/mwdb/epidemics/similarity/DifferenceWordSimilarity.java
+++ Epidemic Data Analytics/src/edu/asu/mwdb/epidemics/similarity/DifferenceWordSimilarity.java
@@ -13,27 +13,28 @@ import edu.asu.mwdb.epidemics.domain.Word;
 public class DifferenceWordSimilarity implements Similarity {
 	private Set<Window> s1, s2;
 
-	public DifferenceWordSimilarity(File f1, File f2) throws IOException {
-		BufferedReader br = new BufferedReader(new FileReader(new File(
-				"epidemic_word_file_diff.csv")));
+	public DifferenceWordSimilarity() throws IOException {
 		s1 = new HashSet<Window>();
 		s2 = new HashSet<Window>();
+	}
+
+	@Override
+	public float getScore(String f1, String f2) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new FileReader(new File(
+				"epidemic_word_file_avg.csv")));
 		String line = "";
 
 		while ((line = br.readLine()) != null) {
 			Word word = new Word(line);
-			if (word.getId().getFileName().equals(f1.getName())) {
+			if (word.getId().getFileName().equals(f1)) {
 				s1.add(word.getWindow());
-			} else if (word.getId().getFileName().equals(f2.getName())) {
+			} else if (word.getId().getFileName().equals(f2)) {
 				s2.add(word.getWindow());
 			}
 		}
 
 		br.close();
-	}
-
-	@Override
-	public float getScore() {
 		Set<Window> cloneS1 = new HashSet<Window>(s1);
 		cloneS1.retainAll(s2);
 		return cloneS1.size();

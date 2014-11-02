1ddd5fbadfbe12e67c69d2b28298753d473e121f
cgarkar <Chandrashekhar.Garkar@asu.edu>
2014-11-02 03:23:16 -0800

    Implemented Pivot

--- Epidemic Data Analytics/src/edu/asu/mwdb/epidemics/fastmap/Pivot.java
+++ Epidemic Data Analytics/src/edu/asu/mwdb/epidemics/fastmap/Pivot.java
@@ -3,14 +3,28 @@ package edu.asu.mwdb.epidemics.fastmap;
 public class Pivot {
 	private int a;
 	private int b;
-	
-	public Pivot(float[][] matrix) {
+	private float distance;
+	public Pivot(DistanceMatrix dm) {
 		//TODO: Fill this constructor with business logic
+		a = getMaxFromRow(dm.getCurrentMatrix(), 0);
+		b = getMaxFromRow(dm.getCurrentMatrix(), a);
+		distance = dm.getCurrentMatrix()[a][b];
 	}
 	
+	private int getMaxFromRow(float[][] currentMatrix, int index) {
+		float max = currentMatrix[index][0];
+		int returnIndex = 0;
+		for(int i = 1; i < currentMatrix.length; i++){
+			if(currentMatrix[i+index][i] > max){
+				max = currentMatrix[i+index][i];
+				returnIndex = i;
+			}
+		}
+		return returnIndex;
+	}
+
 	public float getDistance() {
-		//TODO: Implement this method
-		return 0;
+		return distance;
 	}
 
 	public int getA() {

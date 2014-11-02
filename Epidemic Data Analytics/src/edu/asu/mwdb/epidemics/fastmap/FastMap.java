9b99c5da5a0f62e5e07a0accda480a782604b9d0
Karan Kothari <khkothar@asu.edu>
2014-11-02 16:14:27 -0700

    added a method to get top k similar files

--- Epidemic Data Analytics/src/edu/asu/mwdb/epidemics/fastmap/FastMap.java
+++ Epidemic Data Analytics/src/edu/asu/mwdb/epidemics/fastmap/FastMap.java
@@ -1,59 +1,22 @@
 package edu.asu.mwdb.epidemics.fastmap;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.PriorityQueue;
 
 import edu.asu.mwdb.epidemics.similarity.Similarity;
 
 public class FastMap {
 	
-	/**
-	 * @return the pivotList
-	 */
-	public List<Pivot> getPivotList() {
-		return pivotList;
-	}
-
-	/**
-	 * @param pivotList the pivotList to set
-	 */
-	public void setPivotList(List<Pivot> pivotList) {
-		this.pivotList = pivotList;
-	}
-
-	/**
-	 * @return the distanceMatrix
-	 */
-	public DistanceMatrix getDistanceMatrix() {
-		return distanceMatrix;
-	}
-
-	/**
-	 * @param distanceMatrix the distanceMatrix to set
-	 */
-	public void setDistanceMatrix(DistanceMatrix distanceMatrix) {
-		this.distanceMatrix = distanceMatrix;
-	}
-
-	/**
-	 * @return the reducedMatrix
-	 */
-	public ReducedMatrix getReducedMatrix() {
-		return reducedMatrix;
-	}
-
-	/**
-	 * @param reducedMatrix the reducedMatrix to set
-	 */
-	public void setReducedMatrix(ReducedMatrix reducedMatrix) {
-		this.reducedMatrix = reducedMatrix;
-	}
-
 	private List<Pivot> pivotList;
 	private DistanceMatrix distanceMatrix;
 	private ReducedMatrix reducedMatrix;
+	private Similarity similarityMeasure;
+	private List<String> files;
 	
 	public FastMap(List<String> files, Similarity similarityMeasure, int k) throws Exception {
+		this.files = files;
 		pivotList = new ArrayList<Pivot>();
 		distanceMatrix = new DistanceMatrix(files, similarityMeasure);
 		reducedMatrix = new ReducedMatrix(files.size(), k, distanceMatrix);
@@ -62,6 +25,89 @@ public class FastMap {
 			pivotList.add(pivot);
 			reducedMatrix.addColumn(i, pivot);
 			distanceMatrix.update(reducedMatrix.getMatrix(), pivot);
+		}
+	}
+	
+	public float getError() {
+		float distanceDifference = 0;
+		float sumOfDistanceInOriginalSpace = 0;
+		
+		float[][] distanceMatrixInOrigialSpace = distanceMatrix.getDistanceMatrixInOriginalSpace();
+		float[][] distanceMatrixInReducedSpace = distanceMatrix.getDistanceMatrixInReducedSpace();
+		
+		for(int i = 0; i < distanceMatrixInOrigialSpace.length; i++) {
+			for(int j = 0; j < distanceMatrixInOrigialSpace.length; j++) {
+				distanceDifference = (float) Math.pow(distanceMatrixInOrigialSpace[i][j] - distanceMatrixInReducedSpace[i][j], 2);
+				sumOfDistanceInOriginalSpace = (float) Math.pow(distanceMatrixInOrigialSpace[i][j], 2);
+			}
+		}
+		
+		return (float) Math.sqrt(distanceDifference/sumOfDistanceInOriginalSpace);
+	}
+	
+	public List<String> getTopKSimilarFiles(String query, int k) throws Exception {
+			
+		float modifiedDistance = 0;
+		
+		PriorityQueue<Wrapper> heap = new PriorityQueue<Wrapper>(k, new MinHeap());
+
+		for(int i = 0; i < pivotList.size(); i++) {
+			Pivot pivot = pivotList.get(i);
+			float qa, qb, ab;
+			if(i == 0) {
+				qa = similarityMeasure.getScore(files.get(pivot.getA()), query);
+				qb = similarityMeasure.getScore(files.get(pivot.getB()), query);
+				ab = pivot.getDistance();
+			} else {
+				qa = reducedMatrix.getMatrix()[pivot.getA()][i - 1] - modifiedDistance;
+				qb = reducedMatrix.getMatrix()[pivot.getB()][i - 1] - modifiedDistance;
+				ab = pivot.getDistance();
+			}
+			
+			modifiedDistance = (qa*qa + ab*ab - qb*qb)/2*ab;
+		}
+		
+		for(int i = 0; i < pivotList.size(); i++) {
+			if(heap.size() == k) {
+				heap.poll();
+			}
+			
+			heap.offer(new Wrapper(Math.abs(reducedMatrix.getMatrix()[i][pivotList.size() - 1] - modifiedDistance), i));
+		}
+		
+		List<String> results = new ArrayList<String>();
+		
+		for(int i = 0; i < k; i++) {
+			results.add(files.get(heap.poll().getIndex()));
+		}
+		
+		return results;
+	}
+	
+	private class MinHeap implements Comparator<Wrapper> {
+
+		@Override
+		public int compare(Wrapper o1, Wrapper o2) {
+			return o1.getDistance() <= o2.getDistance() ? 1 : -1;
+		}
+		
+	}
+	
+	private class Wrapper {
+
+		private float distance;
+		private int index;
+		
+		public Wrapper(float distance, int index) {
+			this.distance = distance;
+			this.index = index;
+		}
+		
+		public float getDistance() {
+			return distance;
+		}
+		public int getIndex() {
+			return index;
 		}
 	}
 }

20f2c529d0ccc905fbbc36f1722b3c4fc39932d9
mthorat <mohan.thorat1988@gmail.com>
2014-12-04 02:15:14 -0700

    Created GlobalBitVector class used in VA files and LSH.

---
+++ Epidemic Data Analytics/src/edu/asu/mwdb/epidemics/lsh/Layer.java
@@ -0,0 +1,16 @@
+package edu.asu.mwdb.epidemics.lsh;
+
+import java.util.List;
+import java.util.Map;
+
+public class Layer {
+	
+	private List<Integer> indexPosition;
+	private Map<String,String> hashFamily;
+	
+	public Layer(){}
+	
+	public Layer(List<Integer> indexPositions){
+		
+	}
+}

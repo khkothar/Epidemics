7506fa8ce6a4012a1d110b15c894bdf1cf236f0a
Karan Kothari <khkothar@asu.edu>
2014-12-05 19:27:55 -0700

    implemented DT.. Still Need some testing

--- Epidemic Data Analytics/src/edu/asu/mwdb/epidemics/dt/DecisionTree.java
+++ Epidemic Data Analytics/src/edu/asu/mwdb/epidemics/dt/DecisionTree.java
@@ -1,8 +1,9 @@
 package edu.asu.mwdb.epidemics.dt;
 
-import java.awt.Label;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -10,29 +11,27 @@ public class DecisionTree {
 	
 	private Map<String, int[]> data;
 	private Map<String, Label> trainingSet;
-	private Set<Label> usedLabels;
 	private TreeNode root;
 	
 	public DecisionTree(Map<String, int[]> data, Map<String, Label> trainingSet) {
 		
 		this.data = data;
 		this.trainingSet = trainingSet;
-		usedLabels = new HashSet<Label>();
-		root = createTreeHelper(data);
+		root = createDecisionTreeHelper(data, "");
 		
 	}
 
-	private TreeNode createTreeHelper(Map<String, int[]> remainingData) {
+	private TreeNode createDecisionTreeHelper(Map<String, int[]> remainingData, String usedFeatures) {
 		
 		if(remainingData.size() > data.size() * 0.1) {
 			
-			int featureIndex = getFeatureWithHighestInformationGain(remainingData);
+			int featureIndex = getFeatureWithHighestInformationGain(remainingData, usedFeatures);
 			TreeNode treeNode = new TreeNode();
 			treeNode.featureIndex = featureIndex;
 			Map<String, int[]> leftEntries = getEntries(remainingData, featureIndex, 0);
-			treeNode.left = createTreeHelper(leftEntries);
+			treeNode.left = createDecisionTreeHelper(leftEntries, usedFeatures + featureIndex + ",");
 			Map<String, int[]> rightEntries = getEntries(remainingData, featureIndex, 1);
-			treeNode.right = createTreeHelper(rightEntries);
+			treeNode.right = createDecisionTreeHelper(rightEntries, usedFeatures + featureIndex + ",");
 			return treeNode;
 			
 		} else {
@@ -53,19 +52,128 @@ public class DecisionTree {
 		
 	}
 
-	private int getFeatureWithHighestInformationGain(Map<String, int[]> remainingData) {
+	private int getFeatureWithHighestInformationGain(Map<String, int[]> remainingData, String usedFeatures) {
 		
-		return 0;
+		Set<Integer> usedFeatureSet = parseUsedFeatures(usedFeatures);
+		
+		float entropyOfLabels = calculateEntropy(remainingData.keySet());
+		int vectorSize = 0;
+		for(Map.Entry<String, int[]> entry : remainingData.entrySet()) {
+			vectorSize = entry.getValue().length;
+			break;
+		}
+		
+		float informationGain = Float.MIN_VALUE;
+		int featureIndex = 0;
+		
+		for(int i = 0; i < vectorSize; i++) {
+			
+			if(usedFeatureSet.contains(i)) {
+				continue;
+			}
+			
+			Map<String, int[]> zeroEntries = getEntries(remainingData, i, 0);
+			float zeroEntropy = calculateEntropy(zeroEntries.keySet());
+			Map<String, int[]> oneEntries = getEntries(remainingData, i, 1);
+			float oneEntropy = calculateEntropy(zeroEntries.keySet());
+			
+			float featureEntropy = ((float)zeroEntries.size() / remainingData.size()) * zeroEntropy + 
+					((float)oneEntries.size() / remainingData.size()) * oneEntropy;
+			
+			if(entropyOfLabels - featureEntropy > informationGain) {
+				informationGain = entropyOfLabels - featureEntropy;
+				featureIndex = i;
+			}
+						
+		}
+		
+		
+		return featureIndex;
 	}
 	
+	private Set<Integer> parseUsedFeatures(String usedFeatures) {
+
+		Set<Integer> featureSet = new HashSet<Integer>();
+		
+		if(!usedFeatures.equals("")) {
+			String[] featureArray = usedFeatures.split(",");
+			for(String feature : featureArray) {
+				if(feature != null) {
+					featureSet.add(Integer.parseInt(feature));
+				}
+			}
+		}
+		
+		return featureSet;
+	}
+
+	private float calculateEntropy(Set<String> files) {
+		Map<Label, List<String>> labelMap = new HashMap<Label, List<String>>();
+		
+		for(String file : files) {
+			if(labelMap.containsKey(trainingSet.get(file))) {
+				labelMap.get(trainingSet.get(file)).add(file);
+			} else {
+				List<String> fileList = new ArrayList<String>();
+				fileList.add(file);
+				labelMap.put(trainingSet.get(file), fileList);
+			}
+		}
+		
+		float entropy = 0;
+		
+		for(Map.Entry<Label, List<String>> entry : labelMap.entrySet()) {
+			float weight = (float) (1.0f * entry.getValue().size())/files.size();
+			entropy += weight * (Math.log10(weight) / Math.log10(2));
+		}
+		
+		return entropy * -1;
+	}
+
 	public Map<String, Label> identifyLabel(Map<String, int[]> queries) {
 		
-		TreeNode tempRoot = root;
-		while(tempRoot != null) {
+		Map<String, Label> results = new HashMap<String, Label>();
+		
+		for(Map.Entry<String, int[]> query : queries.entrySet()) {
+			
+			TreeNode tempRoot = root;
+			TreeNode parent = root;
+			int[] queryVector = query.getValue(); 
+			while(tempRoot != null) {
+				if(queryVector[tempRoot.featureIndex] == 0) {
+					parent = tempRoot;
+					tempRoot = tempRoot.left;
+				} else {
+					parent = tempRoot;
+					tempRoot = tempRoot.left;
+				}
+			}
+			
+			Label label = getMaximumRepeatingLabel(parent.files);
+			results.put(query.getKey(), label);
 			
 		}
 		
-		return null;
+		return results;
+		
+	}
+
+	private Label getMaximumRepeatingLabel(List<String> files) {
+
+		Label majorCandidate = trainingSet.get(files.get(0));
+				int counter = 0;
+	    for (int i = 1; i < files.size(); ++i) {
+			if (majorCandidate.getName() == trainingSet.get(files.get(i)).getName()) {
+	            ++counter;
+	        } else if (counter == 0) {
+	            majorCandidate = trainingSet.get(files.get(i));
+	            ++counter;
+	        } else {
+	            --counter;
+	        }
+	    }
+	    return majorCandidate;
+		
 	}
 	
 	
--- Epidemic Data Analytics/src/edu/asu/mwdb/epidemics/dt/Label.java
+++ Epidemic Data Analytics/src/edu/asu/mwdb/epidemics/dt/Label.java
@@ -9,5 +9,29 @@ public class Label {
 	public char getName() {
 		return name;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + name;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Label other = (Label) obj;
+		if (name != other.name)
+			return false;
+		return true;
+	}
+	
+	
 	
 }

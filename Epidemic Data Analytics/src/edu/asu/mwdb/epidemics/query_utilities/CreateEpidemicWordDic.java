76d97868a8819cb90f67e7386302d0f7dc4153bb
cgarkar <Chandrashekhar.Garkar@asu.edu>
2014-12-05 20:37:12 -0700

    bug fix

--- Epidemic Data Analytics/src/edu/asu/mwdb/epidemics/query_utilities/CreateEpidemicWordDic.java
+++ Epidemic Data Analytics/src/edu/asu/mwdb/epidemics/query_utilities/CreateEpidemicWordDic.java
@@ -165,7 +165,8 @@ public class CreateEpidemicWordDic {
 
 		for(int i = 0; i< Constants.NUM_STATES; i++){
 			String stateName = Constants.stateMap.get(i+2);
-			String timeStamp = windowEntries.get(0)[1];
+			Integer iteration = i+1;
+			String timeStamp = iteration.toString();
 			//timeStamp += "'" + windowEntries.get(0)[0];
 			Index idx = new Index(fileName, stateName, timeStamp);
 			String idxString = idx.toString(idx);
